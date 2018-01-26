12268a87d8965af6d6d65edb1298cd9ad987d97a
MovinTarg <34400036+MovinTarg@users.noreply.github.com>
2018-01-26 16:35:30 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-
 $(document).ready(function() {
     $(".back_side").hide();   
 });
-
 $(document).on("click", ".btn", function() {
     $(".users").append("<div class='card'><h1 class='front_side'>"+$("input[name='first_name'").val()+" "+$("input[name='last_name'").val()+"</h1><p class='front_side'>Click for a description!</p><h4 class='back_side'>"+$("textarea[name='description'").val()+"</div>");
     $(".back_side").hide();
+    $("input", "textarea").val("");
     return false;
 });
-
-
 $(document).on("click", ".card", function() {
-    $(this).click(function(){
-        $(".front_side").toggle();
-        $(".back_side").toggle();
-    });
+    $(this).children().toggle();
 });
