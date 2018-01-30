21f607e25878a95eba38d53d8f6bfe6bf06dbe69
MovinTarg <34400036+MovinTarg@users.noreply.github.com>
2018-01-29 19:06:27 -0600

    cleaned up code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 $(document).on("click", ".btn", function() {
     $(".users").append("<div class='card'><h1 class='front_side'>"+$("input[name='first_name'").val()+" "+$("input[name='last_name'").val()+"</h1><p class='front_side'>Click for a description!</p><h4 class='back_side'>"+$("textarea[name='description'").val()+"</div>");
     $(".back_side").hide();
-    $("input", "textarea").val("");
+    $("input, textarea").val("");
     return false;
 });
 $(document).on("click", ".card", function() {
